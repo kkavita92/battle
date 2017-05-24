bb8530575748b40f835c816a761730d222fb721e
Kavita <kkavita92@gmail.com>
2017-05-24 22:57:49 +0100

    Allow players to add their names and view it

--- spec/features/infrastructure_spec.rb
+++ spec/features/infrastructure_spec.rb
@@ -1,6 +1,9 @@
 feature 'First test' do
     scenario 'Print requested string on home page' do
       visit ('/')
-      expect(page).to have_content 'Testing infrastructure working!'
+      fill_in('player1', with: 'Kavita')
+      fill_in('player2', with: 'Prabu')
+      click_button('Submit')
+      expect(page).to have_content 'Kavita VS Prabu'
     end
 end
