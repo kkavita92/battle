9cae1541f7b2ce1f0ba18e87462ff18d80636228
Kavita <kkavita92@gmail.com>
2017-05-25 22:32:29 +0100

    Add functionality: View other player's hit points

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,9 +1,19 @@
-feature 'First test' do
-    scenario 'Print requested string on home page' do
-      visit ('/')
-      fill_in('player1_name', with: 'Kavita')
-      fill_in('player2_name', with: 'Prabu')
-      click_button('Submit')
-      expect(page).to have_content 'Kavita VS Prabu'
-    end
+feature 'Enter and view player names' do
+  scenario 'Print player names' do
+    visit ('/')
+    fill_in('player1_name', with: 'Kavita')
+    fill_in('player2_name', with: 'Prabu')
+    click_button('Submit')
+    expect(page).to have_content 'Kavita VS Prabu'
+  end
+end
+
+feature 'View hit points of players' do
+  scenario 'Print hit points of Player 2' do
+    visit ('/')
+    fill_in('player1_name', with: 'Kavita')
+    fill_in('player2_name', with: 'Prabu')
+    click_button('Submit')
+    expect(page).to have_content 'Prabu: 100HP'
+  end
 end
