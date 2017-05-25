d6d9035e0a8fe3ca414265dc4be190d30352dbe3
Kavita <kkavita92@gmail.com>
2017-05-25 22:38:12 +0100

    Extract code from tests into spec helper

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,19 +1,13 @@
 feature 'Enter and view player names' do
   scenario 'Print player names' do
-    visit ('/')
-    fill_in('player1_name', with: 'Kavita')
-    fill_in('player2_name', with: 'Prabu')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Kavita VS Prabu'
   end
 end
 
 feature 'View hit points of players' do
   scenario 'Print hit points of Player 2' do
-    visit ('/')
-    fill_in('player1_name', with: 'Kavita')
-    fill_in('player2_name', with: 'Prabu')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content 'Prabu: 100HP'
   end
 end
