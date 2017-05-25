eae67a3ff891862bf5dd467efb74080b640e2788
Kavita <kkavita92@gmail.com>
2017-05-25 22:19:39 +0100

    Create POST/redirect/GET pattern

--- spec/features/player_names_spec.rb
+++ spec/features/player_names_spec.rb
@@ -1,8 +1,8 @@
 feature 'First test' do
     scenario 'Print requested string on home page' do
       visit ('/')
-      fill_in('player1', with: 'Kavita')
-      fill_in('player2', with: 'Prabu')
+      fill_in('player1_name', with: 'Kavita')
+      fill_in('player2_name', with: 'Prabu')
       click_button('Submit')
       expect(page).to have_content 'Kavita VS Prabu'
     end
