36723c5ac962d85610576f05c85d9ff9b0e4f894
Kavita <kkavita92@gmail.com>
2017-05-27 15:15:42 +0100

    Change styling of player.erb page

--- spec/features/switch_turn_spec.rb
+++ spec/features/switch_turn_spec.rb
@@ -2,14 +2,14 @@ feature 'Switching turns' do
 
   scenario 'Allow game to start with Player 1' do
     sign_in_and_play
-    expect(page).to have_content 'Turn: Kavita'
+    expect(page).to have_content "It's Kavita's turn now!"
   end
 
   scenario 'Allow game to switch turn from Player 1 to Player 2' do
     sign_in_and_play
     click_button('ATTACK!')
     click_button('OK')
-    expect(page).to have_content 'Turn: Prabu'
+    expect(page).to have_content "It's Prabu's turn now!"
   end
 
 end
